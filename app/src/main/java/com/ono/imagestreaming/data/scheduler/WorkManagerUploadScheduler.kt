647d5946a62d98d4ae9db80861f4b6a65f02cee2
Muhammad Aoun <51170716+MuhammadAounAnwar@users.noreply.github.com>
2024-11-09 03:30:49 +0500

    one timer to periodic

--- app/src/main/java/com/ono/imagestreaming/data/scheduler/WorkManagerUploadScheduler.kt
+++ app/src/main/java/com/ono/imagestreaming/data/scheduler/WorkManagerUploadScheduler.kt
@@ -4,14 +4,17 @@ import android.content.Context
 import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.ono.imagestreaming.domain.scheduler.UploadScheduler
 import com.ono.imagestreaming.ui.scheduler.FrameUploadWorker
+import java.util.concurrent.TimeUnit
 
 class WorkManagerUploadScheduler(private val context: Context) : UploadScheduler {
 
     override fun scheduleUpload() {
-        val workRequest = OneTimeWorkRequestBuilder<FrameUploadWorker>()
+//        val workRequest = OneTimeWorkRequestBuilder<FrameUploadWorker>()
+        val workRequest = PeriodicWorkRequestBuilder<FrameUploadWorker>(15, TimeUnit.MINUTES)
             .setConstraints(
                 Constraints.Builder()
                     .setRequiredNetworkType(NetworkType.CONNECTED)
